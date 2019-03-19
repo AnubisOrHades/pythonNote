bcb4dccefb927bfd9f4556afe9c0dcb039ae51bd
AnubisOrHades <1732306770@qq.com>
2019-03-19 14:24:15 +0800

    get视频

--- Example/my_get.py
+++ Example/my_get.py
@@ -2,18 +2,40 @@ import sys
 from you_get import common as you_get  # 导入you-get库
 
 
-def down_load(path, url):
+def down_load(local_path, url):
     """
     使用you_get下载视频到本地
-    :param path: 保存地址
+    :param local_path: 保存地址
     :param url: 视频连接
     :return:
     """
-    print("开始".center(120, "="))
-    sys.argv = ['you-get', '-o', path, url]  # sys传递参数执行下载，就像在命令行一样
-    you_get.main()
-    print("下载完成".center(120, "="))
+    try:
+        print("开始".center(120, "="))
+        sys.argv = ['you-get', '-o', local_path, url]  # sys传递参数执行下载，就像在命令行一样
+        you_get.main()
+        print("下载完成".center(120, "="))
+    except Exception as e:
+        print(e, "\n", url)
+
+
+def run(local_path, url):
+    t = type(url)
+    if t is list:
+        for u in url:
+            try:
+                down_load(local_path, u)
+            except Exception as e:
+                print("Error:{}\nErrorUrl:{}".format(e, u))
+    elif t is str:
+        try:
+            down_load(local_path, url)
+        except Exception as e:
+            print("Error:{}\nErrorUrl:{}".format(e, url))
+    else:
+        print("url参数错误！！！")
 
 
 if __name__ == '__main__':
-    down_load("D:\\", "http://www.365yg.com/i6650101575308034563/#mid=1618088888118279")
+    path = r"D:\Anubis\Video\socket"
+    urlList = ["https://www.bilibili.com/video/av17732263?p={}".format(i) for i in range(4, 15)]
+    run(path, urlList)
