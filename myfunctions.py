0f697577b93b7b691d68201d50b76f26f5f450fd
AnubisOrHades <1732306770@qq.com>
2019-03-27 17:37:06 +0800

    ...

---
+++ myfunctions.py
@@ -0,0 +1,12 @@
+def for_print(lis):
+    for i in lis:
+        print(i)
+
+
+def print_type(obj):
+    print(obj, "\t", type(obj))
+
+
+def print_dirc(obj):
+    for i in dir(obj):
+        print(i)
