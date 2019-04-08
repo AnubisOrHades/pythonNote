d278acbc74a134d59336dc9e4d45dc194eccc46e
AnubisOrHades <1732306770@qq.com>
2019-04-08 13:45:25 +0800

    添加代理

--- spider/requests/crazy_music.py
+++ spider/requests/crazy_music.py
@@ -1,8 +1,9 @@
 import requests
+from fake_useragent import UserAgent
 
 
 class Music:
-    url = "http://music.ifkdy.com/"
+    host = "http://music.ifkdy.com/"
     header = {
         "Accept": "application/json, text/javascript, */*; q=0.01",
         "Accept-Encoding": "gzip, deflate",
@@ -18,28 +19,59 @@ class Music:
         "X-Requested-With": "XMLHttpRequest"
     }
 
+    proxy = {
+        'HTTPS': '111.177.176.106:9999'
+    }
+
     def __init__(self, kw, t):
         self.keyWord = kw
         self.station = t
 
+    def agent(self):
+        """
+        随机agent
+        :return:
+        """
+        ua = UserAgent()
+        user_agent = ua.random
+        self.header["User-Agent"] = user_agent
+        return self.header
+
     def form_data(self):
-        data = {"input": self.keyWord,
-                "filter": "name",
-                "type": self.station,
-                "page": 1}
+        data = {
+            "input": self.keyWord,
+            "filter": "name",
+            "type": self.station,
+            "page": 1
+        }
         return data
 
     def request_data(self):
-        data = requests.post(self.url, headers=self.header, data=self.form_data()).json()
+        data = requests.post(self.host, proxies=self.proxy, headers=self.agent(), data=self.form_data()).json()
         return data
 
     def down_load(self):
         data = self.request_data()["data"]
-        for misic in data:
-            music_content = requests.get(misic["url"]).content
-            with open("D:\Anubis\Music\多情剑客无情剑\%s.mp3" % misic["title"], "wb")as f:
+        for music in data:
+            music_content = requests.get(music["url"]).content
+            with open("D:\Anubis\Music\多情剑客无情剑\%s.mp3" % music["title"], "wb")as f:
                 f.write(music_content)
 
 
-dd = Music("多情剑客无情剑", "kuwo")
-d = dd.down_load()
+if __name__ == '__main__':
+    music_type = {
+        "网易": "netease",
+        "qq音乐": "qq",
+        "酷我": "kuwo",
+        "酷狗": "kugou",
+        "虾米": "xiami",
+        "百度": "baidu",
+        "咪咕": "migu",
+        "蜻蜓": "qingting",
+        "一听": "1ting",
+        "荔枝": "lizhi",
+        "喜马拉雅": "ximalaya"
+    }
+    dd = Music("美人吟", "netease")
+    d = dd.request_data()
+    print(d)
