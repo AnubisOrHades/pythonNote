2bf85958c6154db14f04d7187cc3e710bcb3b34c
AnubisOrHades <1732306770@qq.com>
2019-05-10 16:47:44 +0800

    python 图片处理

--- Example/img/test.py
+++ Example/img/test.py
@@ -1,8 +1,12 @@
 from PIL import Image
 
-pil_im = Image.open('n.jpg')
+pil_im = Image.open('taotubj.png')
 box = (800, 0, 1100, 300)
 # 剪切图片
 region = pil_im.crop(box)
 
-region.save('new.jpg')
+# region.save('new.jpg')
+
+l = pil_im.convert('L')
+
+l.save('l.jpg')
