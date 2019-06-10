8045cd6934e441809e86517dd3caff3c6dcda2b0
AnubisOrHades <1732306770@qq.com>
2019-06-10 17:09:12 +0800

    格雷码

--- Example/anubis.py
+++ Example/anubis.py
@@ -1,29 +1,19 @@
 def get_password(password):
-    password = bin(password)[2:]
-    print(password)
-    return password
+    # B = list(bin(password)[2:])
+    B = [int(i) for i in bin(password)[2:]]
+    G = []
+    for i in range(len(B)):
+        if i == 0:
+            G.append(B[i])
+        else:
+            G.append(B[i] ^ B[i - 1])
+    # print(B)
+    # print(G)
+    G = [str(i) for i in G]
+    # print("".join(G))
+    # print(int("".join(G), 2))
+    return int("".join(G), 2)
 
 
 if __name__ == '__main__':
-    class GrayCode:
-        def getGray(self, n):
-            # write code here
-            global maxn
-            maxn = n
-            return GrayCode.getGrace(self, ['0', '1'], 1)
-
-        def getGrace(self, list_grace, n):
-            global maxn
-            if n >= maxn:
-                return list_grace
-            list_befor, list_after = [], []
-            for i in range(len(list_grace)):
-                list_befor.append('0' + list_grace[i])
-                list_after.append('1' + list_grace[-(i + 1)])
-            return GrayCode.getGrace(self, list_befor + list_after, n + 1)
-
-
-    # gary = GrayCode()
-    # print("脚本之家测试结果：")
-    # print(gary.getGray(3))
-
+    password = 921224
