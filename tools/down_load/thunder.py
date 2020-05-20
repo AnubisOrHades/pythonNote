a64d52d00b405d330594567acbc13db552fc3610
Anubis <1732306770@qq.com>
2020-05-20 17:30:30 +0800

    添加环境

--- tools/down_load/thunder.py
+++ tools/down_load/thunder.py
@@ -38,13 +38,13 @@ def download(url, re_name=None):
     :return:返回True表示下载完成 否则失败
     """
     os.system(r'"{thunder_path}" {url}'.format(thunder_path=thunder_path, url=url))
-    time.sleep(20)
+    time.sleep(2)
     file_name = get_file_name(url)
     print("正在下载 {}".format(file_name).center(100, "="))
     if check_start(file_name):
         while True:
             # 每分钟检测一次是否下载完成
-            time.sleep(60)
+            time.sleep(6)
             if check_end(file_name):
                 if re_name is not None:
                     old_path = os.path.join(save_path, file_name)
@@ -56,6 +56,11 @@ def download(url, re_name=None):
         return False
 
 
+# 迅雷安装位置
+thunder_path = r"D:\Program Files\Thunder Network\Thunder\Program\Thunder.exe"
+# 文件存储位置，可在迅雷中设置
+save_path = r"F:\迅雷下载\Auto"
+
 if __name__ == '__main__':
     # 迅雷安装位置
     thunder_path = r"D:\Program Files\Thunder Network\Thunder\Program\Thunder.exe"
@@ -65,5 +70,5 @@ if __name__ == '__main__':
     d_url = "https://d2.xia12345.com/down/89/2020/04/mV1qyEda.mp4"
     file_new_name = "小伙子很粗鲁强行扒女友内裤强上还玩轻sm.mp4"
 
-    download(d_url,file_new_name)
+    download(d_url, file_new_name)
     # print(os.path.exists(os.path.join(save_path, get_file_name(d_url))))
