8cd46db02302216cbb1576074ecc64403acadc78
Anubis <1732306770@qq.com>
2020-05-20 17:29:45 +0800

    添加ua代理，文件判断

--- tools/down_load/down_m3u8.py
+++ tools/down_load/down_m3u8.py
@@ -15,7 +15,7 @@ class DownLoadM3U8:
     m3u8_url: str
     file_name: str
     path: str = r"."
-    max_workers = 100
+    max_workers: int = 10
 
     def __post_init__(self):
         self.headers = {
@@ -23,7 +23,7 @@ class DownLoadM3U8:
         self.thread_pool = ThreadPoolExecutor(max_workers=self.max_workers)
 
     def get_ts_url(self):
-        m3u8_obj = m3u8.load(self.m3u8_url)
+        m3u8_obj = m3u8.load(self.m3u8_url, headers=self.headers)
         base_uri = m3u8_obj.base_uri
 
         for seg in m3u8_obj.segments:
@@ -31,15 +31,23 @@ class DownLoadM3U8:
 
     def download_single_ts(self, urlinfo):
         url, ts_name = urlinfo
-        res = requests.get(url, headers=self.headers)
-        with open("{}\\{}".format(self.path, ts_name), 'wb') as fp:
-            fp.write(res.content)
-        print(url)
+        try:
+            file_name = "{}\\{}".format(self.path, ts_name)
+            if os.path.isfile(file_name):
+                return None
+            res = requests.get(url, headers=self.headers)
+            with open(file_name, 'wb') as fp:
+                fp.write(res.content)
+        except Exception as e:
+            print("Error:{}".format(e))
+        else:
+            pass
+        finally:
+            print(url)
 
     def download_all_ts(self):
         ts_urls = self.get_ts_url()
         for index, ts_url in enumerate(ts_urls):
-            # print(ts_url)
             self.thread_pool.submit(self.download_single_ts, [ts_url, f'{index}.ts'])
         self.thread_pool.shutdown()
 
@@ -58,11 +66,12 @@ class DownLoadM3U8:
 
 
 if __name__ == '__main__':
+    start = time.time()
     m3u8_url = 'http://aa.kk400500.com/20200116/BBVZwZdC/438kb/hls/index.m3u8'
     down_path = r'D:'
-    start = time.time()
+    file_name = "亚洲自慰在公共hd上.mp4"
 
-    M3U8 = DownLoadM3U8(m3u8_url, path=down_path, file_name="亚洲自慰在公共hd上.mp4")
+    M3U8 = DownLoadM3U8(m3u8_url, path=down_path, file_name=file_name)
     M3U8.run()
 
     end = time.time()
