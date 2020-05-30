79a353a55bf50061cc29409595b9ffc24f512385
Anubis <1732306770@qq.com>
2020-05-30 22:38:35 +0800

    剪切图片

--- tools/image.py
+++ tools/image.py
@@ -10,3 +10,24 @@ def img_shape(path):
     img = Image.open(path)
     shape = img.size
     return {"width": shape[0], "height": shape[1]}
+
+
+def cut(path_in, path_out, box):
+    """
+    剪切图片并保存
+    :param path_in: 图片路径
+    :param path_out: 剪切后保存路径
+    :param box: 剪切范围 元组类型 (left, upper, right, lower)
+    :return:
+    """
+    try:
+        img = Image.open(path_in)
+        print(img.size)
+        cropped = img.crop(box)  # (left, upper, right, lower)
+        cropped.save(path_out)
+    except Exception as e:
+        print("Error:{}".format(e))
+    else:
+        pass
+    finally:
+        pass
