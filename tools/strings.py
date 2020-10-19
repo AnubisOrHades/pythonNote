a6e6f256a63a67bddbee592b15781b6b7c9ef8b8
AnubisOrHades <1732306770@qq.com>
2020-10-19 11:00:15 +0800

    获取字符串哈希值

--- tools/strings.py
+++ tools/strings.py
@@ -1,4 +1,25 @@
 import requests
+import hashlib
+
+
+def get_str_md5(string):
+    """
+    获取字符串哈希值
+    :param string: 字符串
+    :return: 字符串哈希值
+    """
+    try:
+        md5_obj = hashlib.md5()
+        md5_obj.update(string.encode("utf8"))
+        pass
+    except Exception as e:
+        print("Error:{}".format(e))
+    else:
+        return md5_obj.hexdigest()
+        pass
+    finally:
+        pass
+
 
 def translate(word):
     try:
@@ -12,3 +33,7 @@ def translate(word):
         pass
     finally:
         pass
+
+
+if __name__ == '__main__':
+    pass
