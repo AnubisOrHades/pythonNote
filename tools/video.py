9ed4bdd3f173d344165289db956cee9709f4a4ac
AnubisOrHades <1732306770@qq.com>
2020-11-04 11:28:59 +0800

    捕获异常

--- tools/video.py
+++ tools/video.py
@@ -1,4 +1,6 @@
 import os
+import sys
+import traceback
 
 from moviepy.editor import VideoFileClip
 import cv2
@@ -17,11 +19,19 @@ def preview(video_name, video_path, img_path, video_format="mp4", img_format="jp
     :return: 是否成功（1：成功；0：失败）
     """
     path = '{}\\{}.{}'.format(video_path, video_name, video_format)
+    if not os.path.exists(path):
+        print("Error：{} 不存在".format(path))
+        return 0
     vc = cv2.VideoCapture(path)
 
     if vc.isOpened():
         real, frame = vc.read()
-        cv2.imwrite("{}\\{}.{}".format(img_path, video_name, img_format), frame)
+        img_path = "{}\\{}.{}".format(img_path, video_name, img_format)
+        if os.path.exists(img_path):
+            print("Error：{} 已存在".format(img_path))
+            return 0
+        # cv2.imwrite(img_path, frame) #中文路径乱码
+        cv2.imencode(".{}".format(img_format), frame)[1].tofile(img_path)
         cv2.waitKey(1)
         vc.release()
         return 1
@@ -53,23 +63,34 @@ def compress_video(input_path=None, out_path=None, bitrate=500, ffmpeg_path=None
         pass
 
 
-def get_video_times(filename):
+def get_video_attribute(filename):
     """
-    获取视频时长
+    获取视频属性
     :param filename: 视频时长（单位：秒/s）
     :return: 视频时长
     """
     clip = VideoFileClip(filename)
-    video_time = clip.duration
+    video = {
+        "duration": clip.duration,  # 时长
+        "size": clip.size,  # 尺寸
+        "fps": clip.fps,  # 码率（每秒帧数）
+        "filename": clip.filename  # 名字
+    }
     clip.close()
     # clip.reader.close()
     # clip.audio.reader.close_proc()
-    return video_time
+    return video
 
 
-def de_watermark(file_path, x, y, w, h):
+def de_watermark(file_path, x, y, w, h, out_path=None):
     """
-    去水印
+    去除视频水印
+    :param file_path: 文件完整地址
+    :param x: 水印位置x坐标
+    :param y: 水印位置y坐标
+    :param w: 水印宽度
+    :param h: 水印高度
+    :param out_path:文件输出路径
     :return:
     """
     try:
@@ -77,21 +98,29 @@ def de_watermark(file_path, x, y, w, h):
         old_name = in_file.name
         print(old_name)
         in_file.re_name("in")
-        in_path = "{}\in{}".format(in_file.path, os.path.splitext(in_file.local_path)[1])
-        out_path = "{}\out{}".format(in_file.path, os.path.splitext(in_file.local_path)[1])
+        in_path = "{}\\in{}".format(in_file.path, os.path.splitext(in_file.local_path)[1])
+        if out_path is None:
+            out_path = "{}\\out{}".format(in_file.path, os.path.splitext(in_file.local_path)[1])
+        else:
+            out_path = "{}\\out{}".format(out_path, os.path.splitext(in_file.local_path)[1])
         try:
             os.system("ffmpeg -i {} -vf delogo=x={}:y={}:w={}:h={} {}".format(in_path, x, y, w, h, out_path))
             pass
         except Exception as e:
+            exc_type, exc_value, exc_traceback_obj = sys.exc_info()
+            traceback.print_tb(exc_traceback_obj)
             File(in_path).re_name(old_name)
             print("Error:{}".format(e))
         else:
+            File(in_path).re_name(old_name)
             File(out_path).re_name(old_name)
             pass
         finally:
             pass
         pass
     except Exception as e:
+        exc_type, exc_value, exc_traceback_obj = sys.exc_info()
+        traceback.print_tb(exc_traceback_obj)
         print("Error:{}".format(e))
     else:
         pass
